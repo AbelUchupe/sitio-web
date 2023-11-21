2df408085e963d5e558561e247cf62eb7eb6e69d
Abel Uchupe <99312181+DonlYone1998@users.noreply.github.com>
2023-11-20 19:26:43 -0500

    Se modificó la tabla usando js

--- index.js
+++ index.js
@@ -1,7 +1,19 @@
-var video = document.getElementById("myVideo");
+// var video = document.getElementById("myVideo");
+const tableBody = document.querySelector(".table_body");
 
     // Agregar un evento para reiniciar la reproducción cuando el video llega al final
-    video.addEventListener("ended", function() {
-      video.currentTime = 0; // Reiniciar al principio
-      video.play(); // Volver a reproducir
-    });
+    // video.addEventListener("ended", function() {
+    //   video.currentTime = 0; // Reiniciar al principio
+    //   video.play(); // Volver a reproducir
+    // });
+
+let i = 1;
+
+
+
+while (i<=20) {
+    let rowTable = document.createElement("tr");
+    rowTable.innerHTML = '<td><input class="input" type="text"></td><td><input class="input" type="text"></td><td><input class="input" type="text"></td><td><input class="input" type="text"></td><td><input class="input" type="text"></td><td><input class="input" type="text"></td><td><input class="input" type="text"></td><td><input class="input" type="text"></td>';
+    tableBody.appendChild(rowTable);
+    i=i+1;
+}
